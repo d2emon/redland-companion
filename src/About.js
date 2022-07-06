eefc81c13be15c9a9b60d3fcee06e86e8cad6da7
Dmitry Kutsenko <d2emonium@gmail.com>
2022-07-06 15:52:24 +0300

    Add React Router

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function About() {
+    return <div>
+        Template for about page
+    </div>;
+};
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import About from './About';
 import CharGen from './chargen';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <CharGen />
+      <Routes>
+        <Route path="/" element={<CharGen />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
